7910bb6d4e3ec3461a0d8e5c2d4ba100079a1a5b
Zhaniya110 <zmeyramova@gmail.com>
2023-12-04 03:54:37 +0600

    Kaspi Button Version 2

--- kaspibutton.php
+++ kaspibutton.php
@@ -1,10 +1,8 @@
 <?php 
 /*
- * Plugin Name: Kaspi Button for WooCommerce
+ * Plugin Name: Kaspi Button
  * Author: Zhaniya
  * Author URI: zhan883.kz
- * Description: The Kaspi Button for WooCommerce plugin enhances your online store by seamlessly integrating with Kaspi.kz, providing a streamlined experience for your customers. Easily display essential information under the "Add to Cart" button on both single product pages and product archive (loop) pages.
- * Text Domain: kaspibutton
  */
 
 
@@ -53,73 +51,54 @@ function data_city_callback() {
 }
 
 add_action('admin_init', 'add_custom_settings');
-
-
-function display_custom_link() {
-    global $product;
-
-    $product_sku = get_post_meta($product->get_id(), 'product_sku', true);
-    $merchant_code = get_option('merchant_code');
-    $data_city = get_option('data_city');
-
-    // Output the custom link
-    echo "<div id='dynamic'></div>";
-
-    // Output the script with dynamic values
-    echo "<script>
-        (function (d, s, id) {
-            var js, kjs;
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';
-            kjs = document.getElementsByTagName(s)[0]
-            kjs.parentNode.insertBefore(js, kjs);
-        }(document, 'script', 'KS-Widget'));
-        
-        setTimeout(function () {
-            document.getElementById('dynamic').innerHTML = '<div class=\"ks-widget\" data-template=\"button\" data-merchant-sku=\"$product_sku\" data-merchant-code=\"$merchant_code\" data-city=\"$data_city\"></div>';
-            ksWidgetInitializer.reinit();
-        }, 1000)</script>";
-}
-
-add_action('woocommerce_single_product_summary', 'display_custom_link');
+//scripts
 
 
 function enqueue_custom_scripts() {
     // Enqueue the external script
     wp_enqueue_script('ks-wi-ext-script', 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js', array(), '1.0', true);
 }
-
-// Hook the function to the appropriate action
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
-
-function display_custom_data_under_add_to_cart_loop() {
+function display_custom_data_under_add_to_cart() {
     global $product;
 
+    // Check if product SKU is available
+    
     $product_sku = get_post_meta($product->get_id(), 'product_sku', true);
     $merchant_code = get_option('merchant_code');
     $data_city = get_option('data_city');
+    
+    if (empty($product_sku)) {
+        return; // Skip if SKU is not available
+    }
 
-    // Output the custom link
-    echo "<div id='dynamic'></div>";
+    
 
-    // Output the script with dynamic values
-    echo "<script>
+
+     echo "
+    <div id='dynamic'></div>
+    <script>
         (function (d, s, id) {
             var js, kjs;
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
             js.src = 'https://kaspi.kz/kaspibutton/widget/ks-wi_ext.js';
-            kjs = document.getElementsByTagName(s)[0]
+            kjs = document.getElementsByTagName(s)[0];
             kjs.parentNode.insertBefore(js, kjs);
         }(document, 'script', 'KS-Widget'));
-        
+
         setTimeout(function () {
             document.getElementById('dynamic').innerHTML = '<div class=\"ks-widget\" data-template=\"button\" data-merchant-sku=\"$product_sku\" data-merchant-code=\"$merchant_code\" data-city=\"$data_city\"></div>';
             ksWidgetInitializer.reinit();
-        }, 1000)</script>";
+        }, 1000);
+    </script>
+    ";
 }
 
+// Hook the function to display the custom data under the Add to Cart button
+add_action('woocommerce_single_product_summary', 'display_custom_data_under_add_to_cart', 15); // Adjust the priority to control the position
+
 // Hook the function to display the custom data under the Add to Cart button on the archive loop
-add_action('woocommerce_after_shop_loop_item', 'display_custom_data_under_add_to_cart_loop', 15); // Adjust the priority to control the position
+add_action('woocommerce_after_shop_loop_item', 'display_custom_data_under_add_to_cart', 15); // Adjust the priority to control the position
+?>
